f02088c81735fb0fcb6f19702357a705d5851e3c
tncalpmn <alpmantunc@gmail.com>
2018-02-18 14:07:25 +0100

    Double function Added

--- src/Application.java
+++ src/Application.java
@@ -2,8 +2,20 @@
 public class Application {
 
 	public static void main(String args[]){	
+		
 		System.out.println("First Commit to GitHub");
-				
+		
+		int a = 12;
+	
+		System.out.println(makeDouble(a));
 	}
+	
+	static int makeDouble(int number){
+		
+		return (number *2);
+		
+	}
+	
+	
 	
 }
